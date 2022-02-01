f219388d44e489e4a1cffb553159fc5b34618c73
scalabrindario <scalabrin.dario@gmail.com>
2022-02-02 00:28:50 +0100

    minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
         short_name: `Delog`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#381696`,
+        theme_color: `#1E7A99`,
         display: `standalone`,
         icon: "src/images/icon.png",
       },
