5328c5c189bcbf515804e2960991ff05bdeacd17
scalabrindario <scalabrin.dario@gmail.com>
2022-02-02 23:00:07 +0100

    added HTML of timeline

--- src/pages/page.js
+++ src/pages/page.js
@@ -27,6 +27,74 @@ const NewPage = ({
 
 
       </Helmet>
+        <div class="ag-timeline-block">
+          <section class="ag-section">
+            <div class="ag-format-container">
+              <div class="js-timeline ag-timeline">
+                <div class="js-timeline_line ag-timeline_line">
+                  <div class="js-timeline_line-progress ag-timeline_line-progress"></div>
+                </div>
+                <div class="ag-timeline_list">
+
+
+                  <div class="js-timeline_item ag-timeline_item">
+                    <div class="ag-timeline-card_box">
+                      <div class="js-timeline-card_point-box ag-timeline-card_point-box">
+                        <div class="ag-timeline-card_point">2021</div>
+                      </div>
+                      <div class="ag-timeline-card_meta-box">
+                        <div class="ag-timeline-card_meta">Season 13</div>
+                      </div>
+                    </div>
+                    <div class="ag-timeline-card_item">
+                      <div class="ag-timeline-card_inner">
+                        <div class="ag-timeline-card_img-box">
+                          <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/timeline/images/img-13.png" class="ag-timeline-card_img" width="640" height="360" />
+                        </div>
+                        <div class="ag-timeline-card_info">
+                          <div class="ag-timeline-card_title">Season 13</div>
+                          <div class="ag-timeline-card_desc">
+                            xxxxx
+                          </div>
+                        </div>
+                      </div>
+                      <div class="ag-timeline-card_arrow"></div>
+                    </div>
+                  </div>
+
+                  
+
+                  <div class="js-timeline_item ag-timeline_item">
+                    <div class="ag-timeline-card_box">
+                      <div class="ag-timeline-card_meta-box">
+                        <div class="ag-timeline-card_meta">Season 12</div>
+                      </div>
+                      <div class="js-timeline-card_point-box ag-timeline-card_point-box">
+                        <div class="ag-timeline-card_point">2020</div>
+                      </div>
+                    </div>
+                    <div class="ag-timeline-card_item">
+                      <div class="ag-timeline-card_inner">
+                        <div class="ag-timeline-card_img-box">
+                          <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/timeline/images/img-12.png" class="ag-timeline-card_img" width="640" height="360" alt="" />
+                        </div>
+                        <div class="ag-timeline-card_info">
+                          <div class="ag-timeline-card_title">Season 12</div>
+                          <div class="ag-timeline-card_desc">
+                            xxxxx
+                          </div>
+                        </div>
+                      </div>
+                      <div class="ag-timeline-card_arrow"></div>
+                    </div>
+                  </div>
+
+
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
     </Layout>
   )
 }
