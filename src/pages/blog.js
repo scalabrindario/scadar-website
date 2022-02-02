1c19b32af1f551d5f0c062c25e42c0ef3760859e
scalabrindario <scalabrin.dario@gmail.com>
2022-02-02 10:40:54 +0100

    fixed issue on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -22,7 +22,6 @@ const BlogPage = ({
         <title>{site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
-      <HeroHeader/>
       <h2>Blog Posts &darr;</h2>
       <div className="grids">
         {Posts}
