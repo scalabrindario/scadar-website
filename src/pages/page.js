967cb81100b83ab9250fe508940d41ba0b65318d
scalabrindario <scalabrin.dario@gmail.com>
2022-02-02 22:11:57 +0100

    Try fixing issue on page.js file

--- src/pages/page.js
+++ src/pages/page.js
@@ -13,10 +13,6 @@ const NewPage = ({
   },
 }) => {
 
-  const Posts = edges
-    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
-
   return (
     <Layout>
       <Helmet>
@@ -52,11 +48,7 @@ const NewPage = ({
                       <div class="ag-timeline-card_info">
                         <div class="ag-timeline-card_title">Season 13</div>
                         <div class="ag-timeline-card_desc">
-                          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
-                          eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
-                          montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-                          pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
-                          vel, aliquet nec, vulputate eget, arcu.
+                          xxxxxxx
                         </div>
                       </div>
                     </div>
@@ -81,13 +73,7 @@ const NewPage = ({
                       <div class="ag-timeline-card_info">
                         <div class="ag-timeline-card_title">Season 12</div>
                         <div class="ag-timeline-card_desc">
-                          Donec pede justo, fringilla
-                          vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
-                          venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
-                          tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
-                          tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-                          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
-                          nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+                          xxxxxxx
                         </div>
                       </div>
                     </div>
@@ -112,13 +98,7 @@ const NewPage = ({
                       <div class="ag-timeline-card_info">
                         <div class="ag-timeline-card_title">Season 11</div>
                         <div class="ag-timeline-card_desc">
-                          Aenean imperdiet. Etiam ultricies
-                          nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.
-                          Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet
-                          adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,
-                          hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae
-                          sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget
-                          eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.
+                          xxxxxxx
                         </div>
                       </div>
                     </div>
@@ -135,25 +115,7 @@ const NewPage = ({
                       <div class="ag-timeline-card_point">2019</div>
                     </div>
                   </div>
-                  <div class="ag-timeline-card_item">
-                    <div class="ag-timeline-card_inner">
-                      <div class="ag-timeline-card_img-box">
-                        <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/timeline/images/img-10.png" class="ag-timeline-card_img" width="640" height="360" alt="" />
-                      </div>
-                      <div class="ag-timeline-card_info">
-                        <div class="ag-timeline-card_title">Season 10</div>
-                        <div class="ag-timeline-card_desc">
-                          Cum sociis natoque penatibus et magnis dis parturient
-                          montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-                          pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
-                          vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
-                          venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
-                          tincidunt. Cras dapibus. Vivamus elementum semper nisi.
-                        </div>
-                      </div>
-                    </div>
-                    <div class="ag-timeline-card_arrow"></div>
-                  </div>
+                
                 </div>
 
                 <div class="js-timeline_item ag-timeline_item">
@@ -166,21 +128,6 @@ const NewPage = ({
                     </div>
                   </div>
                   <div class="ag-timeline-card_item">
-                    <div class="ag-timeline-card_inner">
-                      <div class="ag-timeline-card_img-box">
-                        <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/timeline/images/img-9.png" class="ag-timeline-card_img" width="640" height="360" alt="" />
-                      </div>
-                      <div class="ag-timeline-card_info">
-                        <div class="ag-timeline-card_title">Season 9</div>
-                        <div class="ag-timeline-card_desc">
-                          Vivamus elementum semper nisi. Aenean vulputate eleifend
-                          tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-                          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
-                          nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies
-                          nisi vel augue. Curabitur ullamcorper ultricies nisi.
-                        </div>
-                      </div>
-                    </div>
                     <div class="ag-timeline-card_arrow"></div>
                   </div>
                 </div>
@@ -199,18 +146,7 @@ const NewPage = ({
                       <div class="ag-timeline-card_img-box">
                         <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/timeline/images/img-8.png" class="ag-timeline-card_img" width="640" height="360" alt="" />
                       </div>
-                      <div class="ag-timeline-card_info">
-                        <div class="ag-timeline-card_title">Season 8</div>
-                        <div class="ag-timeline-card_desc">
-                          Donec quam felis, ultricies nec, pellentesque eu,
-                          pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
-                          vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
-                          venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
-                          tincidunt. Cras dapibus. Vivamus elementum semper nisi.
-                        </div>
-                      </div>
-                    </div>
-                    <div class="ag-timeline-card_arrow"></div>
+                      
                   </div>
                 </div>
 
@@ -231,11 +167,7 @@ const NewPage = ({
                       <div class="ag-timeline-card_info">
                         <div class="ag-timeline-card_title">Season 7</div>
                         <div class="ag-timeline-card_desc">
-                          Quisque rutrum. Aenean imperdiet. Etiam ultricies
-                          nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.
-                          Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet
-                          adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,
-                          hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus.
+                          xxxxxxx
                         </div>
                       </div>
                     </div>
@@ -260,10 +192,7 @@ const NewPage = ({
                       <div class="ag-timeline-card_info">
                         <div class="ag-timeline-card_title">Season 6</div>
                         <div class="ag-timeline-card_desc">
-                          Vivamus elementum semper nisi. Aenean vulputate eleifend
-                          tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-                          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
-                          nulla ut metus varius laoreet. Quisque rutrum.
+                          xxxxxxx
                         </div>
                       </div>
                     </div>
@@ -371,54 +300,6 @@ const NewPage = ({
                       <div class="ag-timeline-card_point">2015</div>
                     </div>
                   </div>
-                  <div class="ag-timeline-card_item">
-                    <div class="ag-timeline-card_inner">
-                      <div class="ag-timeline-card_img-box">
-                        <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/timeline/images/img-2.png" class="ag-timeline-card_img" width="640" height="360" />
-                      </div>
-                      <div class="ag-timeline-card_info">
-                        <div class="ag-timeline-card_title">Season 2</div>
-                        <div class="ag-timeline-card_desc">
-                          Aenean vulputate eleifend
-                          tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-                          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
-                          nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies
-                          nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
-                        </div>
-                      </div>
-                    </div>
-                    <div class="ag-timeline-card_arrow"></div>
-                  </div>
-                </div>
-
-                <div class="js-timeline_item ag-timeline_item">
-                  <div class="ag-timeline-card_box">
-                    <div class="js-timeline-card_point-box ag-timeline-card_point-box">
-                      <div class="ag-timeline-card_point">2015</div>
-                    </div>
-                    <div class="ag-timeline-card_meta-box">
-                      <div class="ag-timeline-card_meta">Season 1</div>
-                    </div>
-                  </div>
-                  <div class="ag-timeline-card_item">
-                    <div class="ag-timeline-card_inner">
-                      <div class="ag-timeline-card_img-box">
-                        <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/timeline/images/img-1.png" class="ag-timeline-card_img" width="640" height="360" />
-                      </div>
-                      <div class="ag-timeline-card_info">
-                        <div class="ag-timeline-card_title">Season 1</div>
-                        <div class="ag-timeline-card_desc">
-                          Donec pede justo, fringilla
-                          vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
-                          venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
-                          tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
-                          tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-                          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-                        </div>
-                      </div>
-                    </div>
-                    <div class="ag-timeline-card_arrow"></div>
-                  </div>
                 </div>
               </div>
             </div>
@@ -432,13 +313,6 @@ const NewPage = ({
 
 
 
-
-
-
-
-
-
-
 export default NewPage
 export const pageQuery = graphql`
   query NewPageQuery {
@@ -448,19 +322,6 @@ export const pageQuery = graphql`
         description
       }
     }
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-            thumbnail
-          }
-        }
-      }
-    }
   }
+      
 `
