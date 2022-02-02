d08f5373033a290086cbdc53fe5c7b02e3ec318e
scalabrindario <scalabrin.dario@gmail.com>
2022-02-02 22:17:38 +0100

    Try fixing issue on page.js file

--- src/pages/page.js
+++ src/pages/page.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 import { withPrefix, Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import PostLink from "../components/post-link"
+import PostLink from "../components/post-link"
 
 
 
@@ -12,6 +13,10 @@ const NewPage = ({
     allMarkdownRemark: { edges },
   },
 }) => {
+
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
   return (
     <Layout>
@@ -313,15 +318,28 @@ const NewPage = ({
 
 
 
-export default NewPage
+export default IndexPage
 export const pageQuery = graphql`
-  query NewPageQuery {
+  query indexPageQuery {
     site {
       siteMetadata {
         title
         description
       }
     }
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+            thumbnail
+          }
+        }
+      }
+    }
   }
-      
 `
