82f435be98ee68180dc0d49d96ae17067d62c453
scalabrindario <scalabrin.dario@gmail.com>
2022-02-02 10:37:07 +0100

    Added blog page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation">
+    <Link to="/blog">Blog</Link>
   	<Link to="/page">Page</Link>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
