9f7d181b24555bddfa493997e2718c3de20caa14
scalabrindario <scalabrin.dario@gmail.com>
2022-02-03 23:29:09 +0100

    fixed homepage

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -18,9 +18,9 @@ export default () => (
       <div className="hero-header">
         <div className="headline">{data.site.siteMetadata.home.title}</div>
         <div 
-          className="primary-content" 
-          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
-        />
+          className="primary-content"/> 
+          Dario grew up in a family of entrepreneurs in the north of Italy, which nurtured his curiosity towards innovation, technology, and emerging trends. <br/> <br/> He studied in one of the largest Italian Innovation Hubs and worked in a Global Fortune 500 company. These experiences fostered the creation of an open-minded and risk-taker guy, who believes in the power of "doing what is right, not what is easy"
+
         <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
       </div>
     )}
